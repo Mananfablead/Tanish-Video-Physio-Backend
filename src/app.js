f1fab552f4f80ac4cbbcdd2a8a2a274c175c50fb
akash fablead <akash.fablead@gmail.com>
2026-01-21 17:56:07 +0530

    testimonil and video call funcnality in work.

--- src/app.js
+++ src/app.js
@@ -14,7 +14,9 @@ const config = require('./config/env');
 app.use(helmet());
 app.use(cors({
     origin: config.ALLOWED_ORIGINS,
-    credentials: true
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Rate limiting
