494139d7c251c4a94ff9da8997db234d3dff6d68
rohitmfablead <rohit.fablead@gmail.com>
2026-01-22 15:27:15 +0530

    error fix upcomming session

--- src/routes/reports.routes.js
+++ src/routes/reports.routes.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const { getUserReport, getSessionReport, getRevenueReport, getTherapistReport } = require('../controllers/reports.controller');
+const { getUserReport, getSessionReport, getRevenueReport, getTherapistReport, getAdminDashboard } = require('../controllers/reports.controller');
 const { authenticateToken, authorizeRoles } = require('../middlewares/auth.middleware');
 
 const router = express.Router();
@@ -8,5 +8,6 @@ router.get('/users', authenticateToken, authorizeRoles('admin'), getUserReport);
 router.get('/sessions', authenticateToken, authorizeRoles('admin'), getSessionReport);
 router.get('/revenue', authenticateToken, authorizeRoles('admin'), getRevenueReport);
 router.get('/therapists', authenticateToken, authorizeRoles('admin'), getTherapistReport);
+router.get('/admin/dashboard', authenticateToken, authorizeRoles('admin'), getAdminDashboard);
 
 module.exports = router;
