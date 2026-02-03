748e9fa4448d299832d22777b514d1af91c860e0
akash fablead <akash.fablead@gmail.com>
2026-02-03 18:26:35 +0530

    video call in working

--- src/server.js
+++ src/server.js
@@ -108,7 +108,9 @@ app.use(express.urlencoded({ extended: true }));
 //     })
 // );
 
-const UPLOADS_DIR = path.resolve('/home/u378554361/domains/apitanishvideo.fableadtech.in/uploads');
+// const UPLOADS_DIR = path.resolve('/home/u378554361/domains/apitanishvideo.fableadtech.in/uploads');
+const UPLOADS_DIR = path.resolve('/home/u378554361/domains/apitanishvideo.fableadtech.in/public_html');
+
 
 if (!fs.existsSync(UPLOADS_DIR)) {
     fs.mkdirSync(UPLOADS_DIR, { recursive: true });
