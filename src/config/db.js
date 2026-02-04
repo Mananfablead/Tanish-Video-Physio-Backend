a8fd68c1bddc9f0d47ae08ef6dadee35e635748c
akash fablead <akash.fablead@gmail.com>
2026-02-04 12:49:56 +0530

    video call in working

--- src/config/db.js
+++ src/config/db.js
@@ -4,14 +4,7 @@ const config = require('../config/env');
 const connectDB = async () => {
     try {
         // Add connection options for better reliability
-        const conn = await mongoose.connect(config.MONGODB_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true,
-            serverSelectionTimeoutMS: 30000, // Increase server selection timeout to 30 seconds
-            socketTimeoutMS: 45000, // Increase socket timeout
-            connectTimeoutMS: 30000, // Increase connection timeout
-            maxPoolSize: 10, // Maintain up to 10 socket connections
-        });
+        const conn = await mongoose.connect(config.MONGODB_URI);
 
         console.log(`MongoDB Connected: ${conn.connection.host}`);
         console.log(`Database: ${conn.connection.name}`);
