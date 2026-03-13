a0d20eeaead85dfc8b63f2dda4314d5cea0a78a0
rajsingh10 <rajking.singh@gmail.com>
2026-03-13 11:13:41 +0530

    email and whatsapp notifiction problem fix

--- src/middlewares/chatUpload.middleware.js
+++ src/middlewares/chatUpload.middleware.js
@@ -78,7 +78,7 @@ const upload = multer({
     }),
     fileFilter: fileFilter,
     limits: {
-        fileSize: 50 * 1024 * 1024 // 50MB limit for chat attachments
+        fileSize: 100 * 1024 * 1024 // 100MB limit for chat attachments
     }
 });
 
